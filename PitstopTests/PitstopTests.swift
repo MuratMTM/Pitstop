a5f6b589f44993f886f61f8bab18ec58c98c399f
MuratMTM <murat.postmen@gmail.com>
2025-02-21 16:15:26 +0300

    Initial project files

---
+++ PitstopTests/PitstopTests.swift
@@ -0,0 +1,17 @@
+//
+//  PitstopTests.swift
+//  PitstopTests
+//
+//  Created by Murat Işık on 21.02.2025.
+//
+
+import Testing
+@testable import Pitstop
+
+struct PitstopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
