f463908d10c194d1bd90f4d1a4f8f379ae0dc265
MuratMTM <murat.postmen@gmail.com>
2025-02-21 16:15:26 +0300

    Initial project files

---
+++ Pitstop/PitstopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitstopApp.swift
+//  Pitstop
+//
+//  Created by Murat Işık on 21.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PitstopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
