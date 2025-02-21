c73797ddaf53624182d6bbb43cb0d7d44cc2723d
MuratMTM <murat.postmen@gmail.com>
2025-02-21 17:24:51 +0300

    Add initial app model files for MVVM structure

---
+++ Pitstop/Models/Driver.swift
@@ -0,0 +1,19 @@
+//
+//  Driver.swift
+//  Pitstop
+//
+//  Created by Murat Işık on 21.02.2025.
+//
+
+import Foundation
+
+struct Driver: Identifiable, Codable {
+    let id: String
+    let previewImageUrl: String
+    let name: String
+    let number: String
+    let championship: String
+    let points: String
+    let history: String
+    
+}
---
+++ Pitstop/Services/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  Pitstop
+//
+//  Created by Murat Işık on 21.02.2025.
+//
+
+import Foundation
