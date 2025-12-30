bcc215623fc32ab0eef2671fdaf821b3df08da7f
MuratMTM <murat.postmen@gmail.com>
2025-12-30 15:07:30 +0300

    chore(backend): add cron jobs and CORS for production readiness

--- pitstop-news-backend/server.js
+++ pitstop-news-backend/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config({ path: '../.env' });;
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
+app.use(cors()); 
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB başarıyla bağlandı! pitstopdb hazır ✅'))
